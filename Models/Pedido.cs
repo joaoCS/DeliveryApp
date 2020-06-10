86850193c3b08149fc6c437fe89ea036c888e905
JoaoCS <joaoantoniogba025@gmail.com>
2020-06-10 16:09:15 -0300

    Criados modelos e migrações

---
+++ Models/Pedido.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveryApp.Models
+{
+    public class Pedido
+    {
+        public int Id { get; set; }
+        public int PratoId { get; set; }
+        public virtual Prato Prato{ get; set; }
+        public float Preco { get; set; }
+
+        public float Troco { get; set; }
+        public int Quantidade { get; set; }
+        public string Estado { get; set; }
+        public string Cidade { get; set; }
+        public string Endereco { get; set; }
+        public string Numero { get; set; }
+
+    }
+}
